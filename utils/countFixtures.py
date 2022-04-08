4b29d8af37bd680ff92175c0d7dce3e5a98a5793
Nerpheus <goetze.nico@hotmail.de>
2022-04-08 16:04:57 +0200

    logging angepasst

--- utils/countFixtures.py
+++ utils/countFixtures.py
@@ -8,4 +8,4 @@ logger = logging.getLogger(__name__)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    logging.info("Anzahl Fixtures: {}".format(countFixtures()))
+    logging.info("Anzahl Fixtures: {}".format(countFixtures()['COUNT(*)']))
