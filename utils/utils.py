aa5787d93ab14d77a75740b8ad7e2c8a763f3723
Nerpheus <goetze.nico@hotmail.de>
2022-04-10 17:37:31 +0200

    exceptions im downloader ergänzt

--- utils/utils.py
+++ utils/utils.py
@@ -16,23 +16,38 @@ def downloader(url, dateiname):
         'x-rapidapi-host': 'v3.football.api-sports.io'
     }
 
-    retries = 1
+    retries = 0
     success = False
 
     while not success and retries <= 5:
         try:
-            response = requests.get(url=url, headers=headers, allow_redirects=True, timeout=60)
+            response = requests.get(url=url, headers=headers, timeout=60)
             response.encoding = 'utf-8'
+            response.raise_for_status()
             success = response.ok
-            if success and retries > 1:
-                logging.info("Solved!")
-        except requests.exceptions.RequestException:
-            wait = 30 * retries
-            logging.info("Request-Error beim Downloaden! Versuche es in {wait} Sekunden erneut.".format(wait=wait))
-            time.sleep(wait)
-            retries += 1
-        else:
+            if success and retries > 0:
+                logging.info("Solved! {}".format(response.status_code))
             open(dateiname, 'wb').write(response.content)
+        except requests.exceptions.HTTPError as errh:
+            retries += 1
+            wait = 30 * retries
+            logging.info("HTTPError beim Downloaden: {0} in Versuch Nummer {1}".format(errh, retries))
+            time.sleep(wait)
+        except requests.exceptions.ConnectionError as errc:
+            retries += 1
+            wait = 30 * retries
+            logging.info("ConnectionError beim Downloaden: {0} in Versuch Nummer {1}".format(errc, retries))
+            time.sleep(wait)
+        except requests.exceptions.Timeout as errt:
+            retries += 1
+            wait = 30 * retries
+            logging.info("Timeout beim Downloaden: {0} in Versuch Nummer {1}".format(errt, retries))
+            time.sleep(wait)
+        except requests.exceptions.RequestException as err:
+            retries += 1
+            wait = 30 * retries
+            logging.info("RequestException beim Downloaden: {0} in Versuch Nummer {1}".format(err, retries))
+            time.sleep(wait)
 
 
 def abfrage(url):
